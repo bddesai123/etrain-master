db9a399837426d84702db4b2e140840ef698f291
prayaggheeson <prayag.gheeson@gmail.com>
2024-04-09 11:26:22 +0530

    mobile

--- js/contact.js
+++ js/contact.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
                 },
                 number: {
                     required: true,
-                    minlength: 5
+                    minlength: 10
                 },
                 email: {
                     required: true,
@@ -35,22 +35,22 @@ $(document).ready(function(){
             },
             messages: {
                 name: {
-                    required: "come on, you have a name, don't you?",
+                    required: "Enter your Name?",
                     minlength: "your name must consist of at least 2 characters"
                 },
                 subject: {
-                    required: "come on, you have a subject, don't you?",
+                    required: "Give your Subject?",
                     minlength: "your subject must consist of at least 4 characters"
                 },
                 number: {
-                    required: "come on, you have a number, don't you?",
+                    required: "Enter your Mobile Number",
                     minlength: "your Number must consist of at least 5 characters"
                 },
                 email: {
-                    required: "no email, no message"
+                    required: "Enter your email Address"
                 },
                 message: {
-                    required: "um...yea, you have to write something to send this form.",
+                    required: "Given  a message?",
                     minlength: "thats all? really?"
                 }
             },
